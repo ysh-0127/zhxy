dfd7ddba60e23c5e8f6104e115e297be07d3ef7c
ysh <1043701463@qq.com>
2022-10-13 17:54:13 +0800

    年级管理分页查询

--- src/main/java/com/atguigu/myzhxy/controller/GradeController.java
+++ src/main/java/com/atguigu/myzhxy/controller/GradeController.java
@@ -1,8 +1,16 @@
 package com.atguigu.myzhxy.controller;
 
+import com.atguigu.myzhxy.pojo.Grade;
 import com.atguigu.myzhxy.service.GradeService;
+import com.atguigu.myzhxy.util.Result;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +21,18 @@ public class GradeController {
 
     @Autowired
     private GradeService gradeService;
+
+    @ApiOperation("查询年级信息,分页带条件")
+    @GetMapping("/getGrades/{pageNo}/{pageSize}")
+    public Result getGrades(@ApiParam("分页查询页码数") @PathVariable(value = "pageNo") Integer pageNo, // 页码数
+                            @ApiParam("分页查询页大小") @PathVariable(value = "pageSize") Integer pageSize, // 页大小
+                            @ApiParam("分页查询模糊匹配班级名") String gradeName) {
+        //设置分页信息
+        Page<Grade> page = new Page<>(pageNo, pageSize);
+        //调用服务层,传入分页信息,和查询的条件
+        IPage<Grade> pageRs = gradeService.getGradeByOpr(page, gradeName);
+        return Result.ok(pageRs);
+    }
 
 
 }
--- src/main/java/com/atguigu/myzhxy/service/GradeService.java
+++ src/main/java/com/atguigu/myzhxy/service/GradeService.java
@@ -1,8 +1,11 @@
 package com.atguigu.myzhxy.service;
 
 import com.atguigu.myzhxy.pojo.Grade;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 public interface GradeService extends IService<Grade> {
 
+    IPage<Grade> getGradeByOpr(Page<Grade> page, String gradeName);
 }
